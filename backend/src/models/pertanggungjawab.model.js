3f7db503de60f0a6c0d8db7410989256b44ecddc
damiosdam <damiosdam@mail.ugm.ac.id>
2023-11-09 21:55:53 +0700

    penambahan LPJ

---
+++ backend/src/models/pertanggungjawab.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const LPJSchema = new Schema(
+  {
+    namaKegiatan: {
+      type: String,
+    },
+    tempat: {
+      type: String,
+    },
+    waktuKegiatan: {
+      type: Date,
+    },
+    jumlahMahasiswa: {
+      type: Number,
+    },
+    jumlahDosen: {
+      type: Number,
+    },
+    biaya: {
+      type: Number,
+    },
+    biayaAjuan: {
+      type: Number,
+    },
+    narahubung: {
+      type: Number,
+    },
+    fileLPJ: {
+      type: Object,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const LembarPertanggungjawaban = mongoose.model(
+  "LembarPertanggungjawaban",
+  LPJSchema
+);
+module.exports = LembarPertanggungjawaban;
